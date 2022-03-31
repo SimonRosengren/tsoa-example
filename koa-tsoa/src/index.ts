85dd56da94a6b4f4ec91eb9760e3789aa3e5d597
simonrosengren <simon.rosengren@b3skilled.se>
2022-03-31 15:06:02 +0200

    swagger

--- koa-tsoa/src/index.ts
+++ koa-tsoa/src/index.ts
@@ -2,12 +2,23 @@ const Koa = require('koa');
 const Router = require('@koa/router')
 const router = new Router()
 const app = new Koa()
+const { koaSwagger } = require('koa2-swagger-ui')
 const helloController = require('./controllers/UserController')
 const { RegisterRoutes } = require('../build/routes')
-
+const spec = require('../build/swagger.json')
 
 RegisterRoutes(router)
 
 app.use(router.routes()).use(router.allowedMethods());
+
+
+app.use(
+    koaSwagger({
+        routePrefix: '/swagger', // host at /swagger instead of default /docs
+        swaggerOptions: {
+            spec
+        },
+    }),
+);
 
 app.listen(3000, () => console.log('Server is listening...'))
